615145a71638a1f5364f2aec7cbe963540108b33
Elisa Tronetti <elisa.tronetti@studio.unibo.it>
2019-03-18 15:46:31 +0100

    Changed the method setDelta adding an input variable

---
+++ src/implementation/model/game/snake/SpeedPropertyImpl.java
@@ -0,0 +1,59 @@
+package implementation.model.game.snake;
+
+import design.model.game.SpeedProperty;
+
+public class SpeedPropertyImpl implements SpeedProperty {
+
+	private long deltaT;
+	private double multiplier;
+	private long lastUpdate;
+	
+	public SpeedPropertyImpl(long deltaT, double multiplier, long lastUpdate) {
+		this.deltaT = deltaT;
+		this.multiplier = multiplier;
+		this.lastUpdate = lastUpdate;
+	}
+	
+	@Override
+	public long getDeltaT() {
+		return this.deltaT;
+	}
+
+	@Override
+	public void setDeltaT(long deltaT) {
+		this.deltaT = deltaT;
+	}
+
+	@Override
+	public void applySpeedMultiplier(double mul) {
+		this.multiplier = mul;	
+	}
+
+	@Override
+	public double getSpeedMultiplier() {
+		return this.multiplier;
+	}
+
+	@Override
+	public long getLastUpdate() {
+		return this.lastUpdate;
+	}
+
+	@Override
+	public void setLastUpdate(long updateTime) {
+		this.lastUpdate = updateTime;
+	}
+
+	@Override
+	public long getNextUpdate() {
+		return this.lastUpdate + (long)(this.deltaT*this.multiplier);
+	}
+	
+	public String toString() {
+		return "Last update: " + this.lastUpdate + "\n"
+				+ "Delta: " + this.deltaT + "\n"
+				+ "Multiplier: " + this.lastUpdate + "\n"
+				+ "Next update: " + getNextUpdate() + "\n";
+	}
+
+}
