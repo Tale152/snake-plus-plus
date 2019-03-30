34af31d7559769206d8681d0a64bbfc2341ab35b
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-03-30 17:27:34 +0100

    splitted GameStatic into GameSnakesStatic and GameFieldStatic to better suit my style

--- src/implementation/model/game/GameImpl.java
+++ src/implementation/model/game/GameImpl.java
@@ -2,24 +2,22 @@ package implementation.model.game;
 
 import java.util.*;
 import design.model.game.*;
+import implementation.model.game.initializers.Utils;
 import implementation.model.game.items.*;
-import static implementation.model.game.GameStatic.*;
+import static implementation.model.game.GameFieldStatic.*;
+import static implementation.model.game.GameSnakesStatic.*;
 
 public class GameImpl implements Game {
 	
 	private long gameTime;
 	private final Field field;
-	private final List<Snake> snakes;
+	private final List<Snake> snakes = new ArrayList<>();
 	
 	public GameImpl(GameRules gameRules, InitialGameState initialGameState, long gameTime) {
 		check(gameRules, initialGameState, gameTime);
 		this.gameTime = gameTime;
-		
-		snakes = new ArrayList<>();
 		initSnakes(gameRules, initialGameState, snakes, gameTime);
-		
 		field = null; //TODO init field
-		initField();
 	}
 	
 	@Override
@@ -68,6 +66,13 @@ public class GameImpl implements Game {
 					.filter(i -> { return i instanceof Wall; })
 					.forEach(item -> { walls.add(item); });
 		return walls;
+	}
+	
+	private static void check(GameRules gameRules, InitialGameState initialGameState, long gameTime) {
+		Utils.throwNullPointer(gameRules == null || initialGameState == null, "Null args");
+		if (gameTime < 0) {
+			throw new IllegalArgumentException("gameTime cannot be negative");
+		}
 	}
 
 }
