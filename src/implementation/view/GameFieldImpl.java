00f65aa0ef04ec930d566e71e95b4da5b6439498
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-05-10 12:22:40 +0200

    divided Items and snakes into GameField, changed GameViewImpl

--- src/implementation/view/GameFieldImpl.java
+++ src/implementation/view/GameFieldImpl.java
@@ -8,13 +8,18 @@ import javafx.scene.image.Image;
 public class GameFieldImpl implements GameField {
 
 	private Optional<Image> bg;
-	private final Map<Point, List<Image>> spritesMap;
+	private final Map<Point, Image> spritesMap;
+	private final List<Map<Point, Image>> snakeSprites;
 	private final GameViewImpl gw;
 	
-	public GameFieldImpl(GameViewImpl gw) {
+	public GameFieldImpl(GameViewImpl gw, int nPlayer) {
 		this.gw = gw;
 		bg = Optional.empty();
 		spritesMap = new HashMap<>();
+		snakeSprites = new ArrayList<>();
+		for (int i = 0; i < nPlayer; ++i) {
+			snakeSprites.add(new HashMap<>());
+		}
 	}
 	
 	@Override
@@ -27,38 +32,61 @@ public class GameFieldImpl implements GameField {
 		return bg;
 	}
 
-	protected Map<Point, List<Image>> getSpritesMap(){
+	protected Map<Point, Image> getSpritesMap(){
 		return spritesMap;
 	}
 	
 	@Override
-	public List<Image> getCell(Point point) {
+	public Optional<Image> getItemCell(Point point) {
 		if (spritesMap.containsKey(point)) {
-			return spritesMap.get(point);
+			return Optional.of(spritesMap.get(point));
 		}
-		return new ArrayList<>();
+		return Optional.empty();
 	}
 
 	@Override
-	public void addSprite(Point point, Image image) {
-		gw.setDirty();
-		if (spritesMap.containsKey(point)){
-			spritesMap.get(point).add(image);
-		}
-		else {
-			spritesMap.put(point, new ArrayList<Image>(Arrays.asList(image)));
+	public void addItemSprite(Point point, Image image) {
+		if (!spritesMap.containsKey(point)){
+			gw.setDirty();
+			spritesMap.put(point, image);
 		}
 	}
 
 	@Override
-	public void removeSprite(Point point, Image image) {
-		gw.setDirty();
+	public void removeItemSprite(Point point, Image image) {
 		if (spritesMap.containsKey(point)) {
-			spritesMap.get(point).remove(image);
-			if (spritesMap.get(point).isEmpty()) {
-				spritesMap.remove(point);
-			}
+			gw.setDirty();
+			spritesMap.remove(point);
 		}
+	}
+
+	@Override
+	public Map<Point, Image> getSnakeSprites(int playerNumber) {
+		return new HashMap<>(snakeSprites.get(playerNumber));
+	}
+
+	@Override
+	public void addBodyPart(int playerNumber, Point point, Image sprite) {
+		if (!snakeSprites.get(playerNumber).containsKey(point)) {
+			gw.setDirty();
+			snakeSprites.get(playerNumber).put(point, sprite);
+		}
+		
+	}
+
+	@Override
+	public void removeBodyPart(int playerNumer, Point point) {
+		if (snakeSprites.get(playerNumer).containsKey(point)) {
+			gw.setDirty();
+			snakeSprites.get(playerNumer).remove(point);
+		}
+		
+	}
+
+	@Override
+	public void resetSnakeSprites(int playerNumber) {
+		gw.setDirty();
+		snakeSprites.get(playerNumber).clear();
 	}
 
 }
