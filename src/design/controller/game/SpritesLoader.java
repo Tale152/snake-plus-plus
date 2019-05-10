7b0b07241cab11f13c2e481f5c1c9d8fbab413d5
Elisa Tronetti <elisa.tronetti@studio.unibo.it>
2019-05-10 17:23:57 +0200

    Changed SpritesLoader Inteface

--- src/design/controller/game/SpritesLoader.java
+++ src/design/controller/game/SpritesLoader.java
@@ -1,13 +1,13 @@
 package design.controller.game;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
 import java.util.List;
 
 public interface SpritesLoader {
 
-	public List<InputStream> getSnakesSpriteSheet() throws FileNotFoundException;
+	public List<Sprite> getItems();
 	
-	public InputStream getItemsSpriteSheet() throws FileNotFoundException;
+	public List<Sprite> getWalls();
+	
+	public List<List<Sprite>> getSnakes(); 
 	
 }
