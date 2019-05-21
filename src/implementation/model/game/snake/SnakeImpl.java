77034786b8da3f1e60efd628760bfb56eed69421
Elisa Tronetti <elisa.tronetti@studio.unibo.it>
2019-05-21 22:25:53 +0200

    Added the implementation of the constructor

--- src/implementation/model/game/snake/SnakeImpl.java
+++ src/implementation/model/game/snake/SnakeImpl.java
@@ -1,13 +1,14 @@
 package implementation.model.game.snake;
 
 import java.awt.Point;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import design.model.game.BodyPart;
 import design.model.game.Direction;
 import design.model.game.Effect;
+import design.model.game.Field;
 import design.model.game.Player;
 import design.model.game.PlayerNumber;
 import design.model.game.Properties;
@@ -16,93 +17,162 @@ import implementation.model.game.items.BodyPartImpl;
 
 public class SnakeImpl implements Snake{
 
-	private Player player;
-	private Properties properties;
+	private final Player player;
+	private final Properties properties;
+	private final Field field;
 	private List<Effect> effects;
-	private boolean isAlive;
 	private List<BodyPart> bodyPart;
-	private BodyPart firstPart;
+	private boolean isAlive;
 	
-	public SnakeImpl(List<Point> point, PlayerNumber playerNumber, String playerName, Direction direction, long deltaT, double speedMultiplier, long lastUpdate) {
+	public SnakeImpl(PlayerNumber playerNumber, String playerName, Direction direction, 
+			long deltaT, double speedMultiplier, Field field, List<Point> point) {
 		this.player = new PlayerImpl(playerNumber, playerName);
 		this.properties = new PropertiesImpl(direction, deltaT, speedMultiplier);
 		this.effects = new ArrayList<>();
+		this.field = field;
+		this.effects = new ArrayList<>();
+		this.bodyPart = new ArrayList<>();
 		this.isAlive = true;
-		this.bodyPart = new ArrayList<>(); 
-		
-		checkPoint(point);
-		for(int i = 0; i <= point.size() - 1; i++) {
-			this.firstPart = new BodyPartImpl(point.get(i));
-			this.bodyPart.add(firstPart);
+
+		for(int i = 0; i < point.size(); i++) {
+			insertNewPiece(point.get(i));
 		}
-		this.properties.getLengthProperty().lengthen(point.size() - 1); //update length properties, the beginning length is the number of point - 1 (the first is supposed to exist
+		
+		properties.getLengthProperty().lengthen(point.size() - 1); 
 	}
+	
+	private void insertNewPiece(Point point) {
+		int size = this.bodyPart.size();
+		BodyPart p = new BodyPartImpl(point);
+		if(size == 0) {
+			p.setHead(true);
+			p.setTail(true);
+		} else if (size == 1) {
+			p.setTail(true);
+			this.bodyPart.get(size - 1).setTail(false);
+			bodyPartCombination(p);
+		} else {
+				p.setTail(true);
+				bodyPartCombination(p);
+				this.bodyPart.get(size - 1).setTail(false);
+				this.bodyPart.get(size - 1).setBody(true);
+			
+		}
+		this.bodyPart.add(p);
+	}
+	
+	//una volta controllato l'algoritmo sotto risistema i collegamente di bodypart
+	private void bodyPartCombination(BodyPart part) {
+		int size = this.bodyPart.size();
+		switch(determinateDirection(this.bodyPart.get(size - 1).getPoint(), part.getPoint())) {
+		case UP : 
+			this.bodyPart.get(size - 1).setCombinedOnTop(true); 
+			part.setCombinedOnBottom(true); 
+			break;
+		case DOWN : 
+			this.bodyPart.get(size - 1).setCombinedOnBottom(true); 
+			part.setCombinedOnTop(true); 
+			break;
+		case LEFT :
+			this.bodyPart.get(size - 1).setCombinedOnRight(true); 
+			part.setCombinedOnLeft(true); 
+			break;
+		case RIGHT : 
+			this.bodyPart.get(size - 1).setCombinedOnLeft(true);
+			part.setCombinedOnRight(true);
+			break;
+		}
+	}
+	
 	
 	@Override
 	public void run() {
+		while(isAlive) {
+
+			for(BodyPart b : this.bodyPart) {
+				System.out.println( "Punto: " + b.getPoint() + "\n" 
+						+ "Is Head: " + b.isHead() +  "\n"
+						+ "Is Tail: " + b.isTail() + "\n"
+						+ "Is Body: " + b.isBody() + "\n"
+						+ "Is connected on top: " + b.isCombinedOnTop() + "\n"
+						+ "Is connected on right: " + b.isCombinedOnRight() + "\n"
+						+ "Is connected on bottom: " + b.isCombinedOnBottom() + "\n"
+						+ "Is connected on left: " + b.isCombinedOnLeft() + "\n\n");
+			}
+			
+		}	
+	}
+
+	@Override
+	public Player getPlayer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Properties getProperties() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addEffect(Effect effect) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public Player getPlayer() {
-		return this.player;
-	}
-
-	@Override
-	public Properties getProperties() {
-		return this.properties;
-	}
-
-	@Override
-	public void addEffect(Effect effect) {
-		this.effects.add(effect);
-	}
-
-	@Override
 	public boolean removeEffect(Effect effect) {
-		return this.effects.remove(effect);
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public List<Effect> getEffects() {
-		return new ArrayList<>(effects);
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
 	public boolean isAlive() {
-		return this.isAlive;
+		// TODO Auto-generated method stub
+		return false;
 	}
 
 	@Override
 	public void kill() {
-		this.isAlive = false;
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public void reverse() {
-		Direction direction;
-		if(this.bodyPart.size() > 1) {
-			//le coordinate della testa sono le coordinate della coda ecc
-			//inverte le coordinate dei pezzi 
-			//e poi setta la direzione dello snake 
-			Point p1 = this.bodyPart.get(this.bodyPart.size() - 2).getPoint();
-			Point p2 = this.bodyPart.get(this.bodyPart.size() - 1).getPoint();
-			//determino e setto la nuova direzione dello snake 
-			direction = determinateDirection(p1, p2);
-			Collections.reverse(this.bodyPart);
-		} else {
-			direction = determinateOppositeDirection(this.properties.getDirectionProperty().getDirection()); //calcolo la direzione opposta se snake ha lunghezza 1
-		}
-		this.properties.getDirectionProperty().forceDirection(direction);
-
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
 	public List<BodyPart> getBodyParts() {
-		return new ArrayList<BodyPart>(this.bodyPart);
+		// TODO Auto-generated method stub
+		return null;
 	}
 
+	private Direction determinateOppositeDirection(Direction d) {
+		Direction updatedDirection;
+		
+		switch(d) {
+		case UP: updatedDirection = Direction.DOWN; break;
+		case DOWN: updatedDirection = Direction.UP; break;
+		case RIGHT: updatedDirection = Direction.LEFT; break;
+		case LEFT: updatedDirection = Direction.RIGHT; break;
+		default: throw new IllegalStateException();	
+		}
+		
+		return updatedDirection;
+	}
+	
+	
+	//TODO controlla l'algoritmo, penso proprio sia sbagliato di base
 	private Direction determinateDirection(Point p1, Point p2) {
 		//if x are the same Snake is moving up or down
 		if(p1.x == p2.x) {
@@ -139,62 +209,4 @@ public class SnakeImpl implements Snake{
 		}
 		throw new IllegalStateException();
 	}
-	
-	
-	private Direction determinateOppositeDirection(Direction d) {
-		Direction updatedDirection;
-		
-		switch(d) {
-		case UP: updatedDirection = Direction.DOWN; break;
-		case DOWN: updatedDirection = Direction.UP; break;
-		case RIGHT: updatedDirection = Direction.LEFT; break;
-		case LEFT: updatedDirection = Direction.RIGHT; break;
-		default: throw new IllegalStateException();	
-		}
-		
-		return updatedDirection;
-	}
-	
-	
-	private void checkPoint(List<Point> point) {
-		if(point.size() < 0) {
-			throw new IllegalArgumentException(); 
-		}
-	}
-	
-	public String toString() {
-		return "Snake is alive: " + this.isAlive + "\n";
-	}
-	
-//LA MOVE È DA RIFARE
-	
-//	private List<Item> move(Point point) {
-//		final int currentLength = this.properties.getLengthProperty().getLength();
-//		final Point oldTail;
-//		List<Item> differences = new ArrayList<>();
-//		
-//		//if Snake is longer than the size he supposed to be
-//		if (this.bodyPart.size() > currentLength) {
-//			while(this.bodyPart.size() != currentLength) {
-//				differences.add(this.bodyPart.get(this.bodyPart.size() - 1));
-//				this.bodyPart.remove(this.bodyPart.size() - 1);
-//			}
-//		}
-//		//calcola le nuove posizioni di ogni pezzo ma si salva la vecchia posizione della coda 
-//		oldTail = new Point(this.bodyPart.get(this.bodyPart.size() - 1).getPoint().x, this.bodyPart.get(this.bodyPart.size() - 1).getPoint().y);
-//		for(int i = this.bodyPart.size() - 1; i < 0; i--) {
-//			this.bodyPart.get(i).getPoint().move(this.bodyPart.get(i-1).getPoint().x, this.bodyPart.get(i-1).getPoint().y);
-//		}
-//		
-//		this.bodyPart.get(0).getPoint().move(point.x, point.y);
-//		
-//		//crea al massimo un nuovo pezzo assegnandogli le coordinate della vecchia coda
-//		if(this.bodyPart.size() < currentLength) {
-//			BodyPart b = (BodyPart) ItemFactory.createBodyPart(oldTail, this);
-//			this.bodyPart.add(b);
-//			differences.add(this.bodyPart.get(this.bodyPart.size() - 1));
-//		}
-//		
-//		return differences;
-//	}
 }
