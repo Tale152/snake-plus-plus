74ce798cb4fec00f0614f1050c6da5cd0dd89d06
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-05-24 01:02:46 +0200

    implemented BadApple, ScoreEarning, ScoreLoss

--- src/implementation/model/game/items/ScoreEarning.java
+++ src/implementation/model/game/items/ScoreEarning.java
@@ -1,6 +1,8 @@
 package implementation.model.game.items;
 
 import java.util.Optional;
+
+import design.model.game.Effect;
 import design.model.game.Field;
 import design.model.game.Snake;
 
@@ -8,31 +10,32 @@ public class ScoreEarning extends EffectAbstract{
 
 	public ScoreEarning(Optional<Long> dEffectDuration) {
 		super(dEffectDuration);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public void instantaneousEffect(Snake target) {
-		// TODO Auto-generated method stub
-		
+		Optional<Effect> active = target.getEffects().stream().filter(e -> {return e.getClass().equals(this.getClass());}).findFirst();
+		if (active.isPresent()) {
+			target.getPlayer().addScore(Apple.SCORE_INCREMENT * (active.get().getComboCounter() + 1));
+		}
+		else {
+			target.getPlayer().addScore(Apple.SCORE_INCREMENT);
+		}
 	}
 
 	@Override
 	public void expirationEffect(Field field) {
-		// TODO Auto-generated method stub
-		
+		// does nothing
 	}
 
 	@Override
-	protected void behaviorOnLastingEffectStart() {
-		// TODO Auto-generated method stub
-		
+	protected void behaviorOnLastingEffectStart(Snake snake) {
+		// does nothing
 	}
 
 	@Override
-	protected void behaviorOnLastingEffectEnd() {
-		// TODO Auto-generated method stub
-		
+	protected void behaviorOnLastingEffectEnd(Snake snake) {
+		// does nothing
 	}
 	
 }
