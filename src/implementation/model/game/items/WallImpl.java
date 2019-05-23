429086f8a6b6e3eeabf24aee892452bbbfee7c61
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-05-23 23:55:43 +0200

    fixed wall to call reverse on snake if snake has spring active

--- src/implementation/model/game/items/WallImpl.java
+++ src/implementation/model/game/items/WallImpl.java
@@ -13,9 +13,11 @@ public class WallImpl extends CollidableAbstract implements Wall{
 	@Override
 	public void onCollision(Snake collider) throws NoSuchMethodException, SecurityException, InstantiationException,
 			IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		if (!collider.getProperties().getCollisionProperty().getIntangibility() &&
-				!collider.getProperties().getCollisionProperty().getInvincibility() &&
-				!collider.getProperties().getCollisionProperty().getSpring()) {
+		if (collider.getProperties().getCollisionProperty().getSpring()) {
+			collider.reverse();
+		}
+		else if (!collider.getProperties().getCollisionProperty().getIntangibility() &&
+				!collider.getProperties().getCollisionProperty().getInvincibility()) {
 			collider.kill();
 		}
 	}
