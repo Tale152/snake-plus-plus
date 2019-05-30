116951a4544a756786947a9869ec0912672df0c7
Elisa Tronetti <elisa.tronetti@studio.unibo.it>
2019-05-30 11:41:37 +0200

    Updated gameController methods

--- src/implementation/controller/game/GameControllerImpl.java
+++ src/implementation/controller/game/GameControllerImpl.java
@@ -10,14 +10,19 @@ import design.controller.game.GameLoader;
 import design.controller.game.InputEvent;
 import design.controller.game.ItemCounter;
 import design.model.game.DirectionProperty;
+import design.model.game.Field;
 import design.model.game.GameModel;
+import design.model.game.Item;
+import design.model.game.ItemRule;
+import design.model.game.LossConditions;
 import design.model.game.Snake;
+import design.model.game.WinConditions;
 import design.view.game.GameView;
 import design.view.game.ResourcesLoader;
 
 public class GameControllerImpl implements GameController {
 	
-	private final ItemCounter gameItems;
+	private final ItemCounter counter;
 	private final GameView gameView;
 	private final GameModel gameModel;
 	private final ResourcesLoader resources;
@@ -31,7 +36,11 @@ public class GameControllerImpl implements GameController {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		this.gameModel.getField().begin();
+		while(!isGameEnded()) {
+			updateDeletedItems();
+			spawnItems();
+		}
 
 	}
 
@@ -43,6 +52,31 @@ public class GameControllerImpl implements GameController {
 			Snake target = gameModel.getField().getSnakes().get(action.get().getPlayerNumber().ordinal());
 			DirectionProperty direction = target.getProperties().getDirectionProperty();
 			direction.setDirection(action.get().getDirection());
+		}
+	}
+	
+	private boolean isGameEnded() {
+		WinConditions win = this.gameModel.getGameRules().getWinConditions();
+		LossConditions loss = this.gameModel.getGameRules().getLossConditions();
+		List<Snake> snakes = this.gameModel.getField().getSnakes();
+		Long time = System.currentTimeMillis();
+		return loss.checkSnakes(snakes) || loss.checkTime(time) ||
+				win.checkScore(snakes) || win.checkSnakeLength(snakes) || win.checkTime(time);
+	}
+	
+	private void updateDeletedItems() {
+		List<Item> deletedItems = this.gameModel.getField().getEliminatedItems();
+		for(Item i : deletedItems) {
+			this.counter.decrease(i.getEffectClass());
+			this.gameView.getField().removeItemSprite(i.getPoint(), this.resources.getItem(i.getClass().getSimpleName()));
+		}
+	}
+	
+	private void spawnItems() {
+		for(ItemRule rule : this.gameModel.getGameRules().getItemRules()) {
+			if(!this.counter.isMax(rule.getEffectClass())) {
+				
+			}
 		}
 	}
 
