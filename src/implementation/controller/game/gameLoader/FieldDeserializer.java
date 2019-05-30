415e398e54f9f27cb70e5d855bf5495978afacf8
Nixola <nicolaorlando24@gmail.com>
2019-05-30 12:41:13 +0200

    Added field deserializer

--- src/implementation/controller/game/gameLoader/FieldDeserializer.java
+++ src/implementation/controller/game/gameLoader/FieldDeserializer.java
@@ -2,22 +2,21 @@ package implementation.controller.game.gameLoader;
 
 import java.awt.Point;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.Optional;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 
+import design.model.game.Effect;
 import design.model.game.Field;
 import design.model.game.Wall;
 import implementation.model.game.field.FieldImpl;
-import implementation.model.game.items.WallImpl;
+import implementation.model.game.items.ItemFactory;
 
 
 /*
@@ -51,13 +50,33 @@ class FieldDeserializer extends StdDeserializer<Field> {
 	
 	public Field deserialize(JsonParser parser, DeserializationContext deserializer) throws IOException {
 		JsonNode node = parser.getCodec().readTree(parser);
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule(new Jdk8Module());
 		
 		Point fieldSize = node.get("dimensions").traverse().readValueAs(Point.class);
 		Field f = new FieldImpl(fieldSize);
+		ItemFactory itemFactory = new ItemFactory(f);
 		
 		JsonNode walls = node.get("walls");
 		for (final JsonNode wall : walls) {
 			f.addWall(wall.traverse().readValueAs(Wall.class));
+		}
+		
+		JsonNode items = node.get("items");
+		
+		for (final JsonNode item : items) {
+			Point position = item.get("position").traverse().readValueAs(Point.class);
+			
+			Class<? extends Effect> effect = item.get("effect").traverse().readValueAs(new TypeReference<Effect>() {});
+			
+			//TODO: read Optionals instead of ternary
+			Optional<Long> itemDuration;
+			itemDuration = item.get("itemDuration").isLong() ? Optional.of(item.get("itemDuration").asLong()) : Optional.empty();
+			
+			Optional<Long> effectDuration;
+			effectDuration = item.get("effectDuration").isLong() ? Optional.of(item.get("effectDuration").asLong()) : Optional.empty();
+			
+			itemFactory.createItem(position, effect, itemDuration, effectDuration);
 		}
 		
 		return f;
--- src/implementation/controller/game/gameLoader/GameLoaderJSON.java
+++ src/implementation/controller/game/gameLoader/GameLoaderJSON.java
@@ -1,27 +1,19 @@
 package implementation.controller.game.gameLoader;
 
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import design.controller.game.GameLoader;
 import design.model.game.Field;
@@ -31,13 +23,11 @@ import design.model.game.ItemRule;
 import design.model.game.LossConditions;
 import design.model.game.WinConditions;
 import implementation.model.game.GameModelImpl;
-import implementation.model.game.field.FieldImpl;
 import implementation.model.game.gameRules.GameRulesImpl;
 import implementation.model.game.gameRules.ItemRuleImpl;
 import implementation.model.game.gameRules.LossConditionsImpl;
 import implementation.model.game.gameRules.WinConditionsImpl;
 import implementation.model.game.items.Apple;
-import implementation.model.game.items.WallImpl;
 
 public class GameLoaderJSON implements GameLoader {
 	
@@ -58,32 +48,16 @@ public class GameLoaderJSON implements GameLoader {
 		return json;
 	}
 	
-	private Field loadField(JsonNode json) throws JsonParseException, JsonMappingException, IOException {
-		Point size = objectMapper.readValue(json.get("dimensions").traverse(), Point.class);
-		Field field = new FieldImpl(size);
-		String map = json.get("map").asText();
-		List<String> lines = Arrays.asList(map.split("\n"));
-		
-		for (int y = 0; y < lines.size(); y++) {
-			String line = lines.get(y);
-			while (line.matches("#")) {
-				int x = line.indexOf('#');
-				field.addWall(new WallImpl(new Point(x, y)));
-				line = line.substring(x + 1);
-			}
-		}
-		
-		return field;
-	}
+	
 	
 	public GameLoaderJSON(String stagePath) throws IOException {
 		objectMapper = new ObjectMapper();
 		String json = readJSON(stagePath);
 		
 		JsonNode loader = objectMapper.readTree(json);
-		Field field = loadField(loader.get("field"));
+		Field field = loader.get("field").traverse().readValueAs(Field.class);
 		
-		GameRules rules = objectMapper.readValue(loader.get("rules").traverse(), GameRules.class);
+		GameRules rules = loader.get("rules").traverse().readValueAs(GameRules.class);
 		
 		this.gameModel = new GameModelImpl(field, rules);
 	}
