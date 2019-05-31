30574caad0e2e4e810c2f7b9e71a2b1fe564a850
Elisa Tronetti <elisa.tronetti@studio.unibo.it>
2019-05-31 11:44:49 +0200

    Fixed addEffect method

--- src/implementation/model/game/snake/SnakeImpl.java
+++ src/implementation/model/game/snake/SnakeImpl.java
@@ -79,7 +79,7 @@ public class SnakeImpl implements Snake{
 	public void addEffect(Effect effect) {
 		if(!this.effects.contains(effect)) {
 			this.effects.add(effect);
-			effect.run();
+			new Thread(effect).start();
 		} else {
 			effect.incrementDuration(effect.getEffectDuration().get());
 		}
