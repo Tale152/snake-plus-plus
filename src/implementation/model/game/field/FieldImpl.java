baa3368742b0da404c5ebe2f0df4fba61a148123
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-06-02 16:12:44 +0200

    synchronized all methods and used synchronized lists

--- src/implementation/model/game/field/FieldImpl.java
+++ src/implementation/model/game/field/FieldImpl.java
@@ -1,16 +1,8 @@
 package implementation.model.game.field;
 
 import java.awt.Point;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import design.model.game.BodyPart;
-import design.model.game.Collidable;
-import design.model.game.Field;
-import design.model.game.Item;
-import design.model.game.Snake;
-import design.model.game.Wall;
+import java.util.*;
+import design.model.game.*;
 
 public class FieldImpl implements Field {
 	
@@ -30,12 +22,12 @@ public class FieldImpl implements Field {
 	private boolean begun;
 
 	public FieldImpl(Point dimensions) {
-		items = new ArrayList<Item>();
-		walls = new ArrayList<Wall>();
-		bodyParts = new ArrayList<BodyPart>();
-		snakes = new ArrayList<Snake>();
-		removedItems = new ArrayList<Item>();
-		threads = new ArrayList<Thread>();
+		items = Collections.synchronizedList(new ArrayList<Item>());
+		walls = Collections.synchronizedList(new ArrayList<Wall>());
+		bodyParts = Collections.synchronizedList(new ArrayList<BodyPart>());
+		snakes = Collections.synchronizedList(new ArrayList<Snake>());
+		removedItems = Collections.synchronizedList(new ArrayList<Item>());
+		threads = Collections.synchronizedList(new ArrayList<Thread>());
 		
 		begun = false;
 		
@@ -43,7 +35,7 @@ public class FieldImpl implements Field {
 		this.height = (int) dimensions.getY();
 	}
 	
-	private boolean contains(Collidable item) {
+	private synchronized boolean contains(Collidable item) {
 		Point coord = item.getPoint();
 		int x = (int) coord.getX();
 		int y = (int) coord.getY();
@@ -53,7 +45,7 @@ public class FieldImpl implements Field {
 		return false;
 	}
 	
-	private boolean isCollidableAddable(Collidable item, List<? extends Collidable> list) {
+	private synchronized boolean isCollidableAddable(Collidable item, List<? extends Collidable> list) {
 		if (list.contains(item)) {
 			return false;
 		} else {
@@ -65,29 +57,29 @@ public class FieldImpl implements Field {
 		}
 	}
 	
-	private void addThread(Runnable runnable) {
+	private synchronized void addThread(Runnable runnable) {
 		Thread thread = new Thread(runnable);
 		thread.start();
 		threads.add(thread);
 	}
 
 	@Override
-	public int getWidth() {
+	public synchronized int getWidth() {
 		return width;
 	}
 
 	@Override
-	public int getHeight() {
+	public synchronized int getHeight() {
 		return height;
 	}
 
 	@Override
-	public List<Item> getItems() {
+	public synchronized List<Item> getItems() {
 		return new ArrayList<Item>(items);
 	}
 
 	@Override
-	public boolean removeItem(Item item) {
+	public synchronized boolean removeItem(Item item) {
 		if (items.remove(item)) {
 			removedItems.add(item);
 			return true;
@@ -96,7 +88,7 @@ public class FieldImpl implements Field {
 	}
 
 	@Override
-	public boolean addItem(Item item) throws IllegalStateException {
+	public synchronized boolean addItem(Item item) throws IllegalStateException {
 		if (this.isCollidableAddable(item, items)) {
 			items.add(item);
 			if (begun) {
@@ -108,16 +100,16 @@ public class FieldImpl implements Field {
 	}
 
 	@Override
-	public List<Collidable> getCell(Point point) {
+	public synchronized List<Collidable> getCell(Point point) {
 		List<Collidable> cellList = new ArrayList<Collidable>();
-		cellList.addAll(Arrays.asList(items.stream().filter(i -> i.getPoint().equals(point)).toArray(Collidable[]::new)));
-		cellList.addAll(Arrays.asList(walls.stream().filter(i -> i.getPoint().equals(point)).toArray(Collidable[]::new)));
-		cellList.addAll(Arrays.asList(bodyParts.stream().filter(i -> i.getPoint().equals(point)).toArray(Collidable[]::new)));
+		items.stream().filter(i -> i.getPoint().equals(point)).forEach(Item -> cellList.add(Item));
+		walls.stream().filter(i -> i.getPoint().equals(point)).forEach(Wall -> cellList.add(Wall));
+		bodyParts.stream().filter(i -> i.getPoint().equals(point)).forEach(BodyPart -> cellList.add(BodyPart));
 		return cellList;
 	}
 
 	@Override
-	public void begin() {
+	public synchronized void begin() {
 		for (Snake snake : snakes) {
 			addThread(snake);
 		}
@@ -130,19 +122,19 @@ public class FieldImpl implements Field {
 	}
 
 	@Override
-	public List<Item> getEliminatedItems() {
+	public synchronized List<Item> getEliminatedItems() {
 		List<Item> returnedList = new ArrayList<Item>(removedItems);
 		removedItems.clear();
 		return returnedList;
 	}
 
 	@Override
-	public List<Wall> getWalls() {
+	public synchronized List<Wall> getWalls() {
 		return new ArrayList<Wall>(walls);
 	}
 
 	@Override
-	public boolean addWall(Wall wall) throws IllegalStateException {
+	public synchronized boolean addWall(Wall wall) throws IllegalStateException {
 		if (this.isCollidableAddable(wall, walls)) {
 			walls.add(wall);
 			return true;
@@ -151,12 +143,12 @@ public class FieldImpl implements Field {
 	}
 
 	@Override
-	public List<BodyPart> getBodyParts() {
+	public synchronized List<BodyPart> getBodyParts() {
 		return new ArrayList<BodyPart>(bodyParts);
 	}
 
 	@Override
-	public boolean addBodyPart(BodyPart bodyPart) throws IllegalStateException {
+	public synchronized boolean addBodyPart(BodyPart bodyPart) throws IllegalStateException {
 		if (this.isCollidableAddable(bodyPart, bodyParts)) {
 			bodyParts.add(bodyPart);
 			return true;
@@ -165,17 +157,17 @@ public class FieldImpl implements Field {
 	}
 
 	@Override
-	public boolean removeBodyPart(BodyPart bodyPart) {
+	public synchronized boolean removeBodyPart(BodyPart bodyPart) {
 		return bodyParts.remove(bodyPart);
 	}
 
 	@Override
-	public List<Snake> getSnakes() {
+	public synchronized List<Snake> getSnakes() {
 		return new ArrayList<Snake>(snakes);
 	}
 
 	@Override
-	public boolean addSnake(Snake snake) {
+	public synchronized boolean addSnake(Snake snake) {
 		if (snakes.contains(snake)) {
 			return false;
 		}
