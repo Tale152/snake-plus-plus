f84391ef5afdf968313a99dcfed09b23b38a3ec4
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-07-30 17:00:29 +0200

    implemented methods to handle sprites into PlayerHudImpl

--- src/implementation/view/game/PlayerHudImpl.java
+++ src/implementation/view/game/PlayerHudImpl.java
@@ -1,17 +1,24 @@
 package implementation.view.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import design.view.game.PlayerHud;
+import design.view.game.Sprite;
 
 public class PlayerHudImpl implements PlayerHud {
 
 	private String name;
 	private String score;
 	private boolean alive;
+	private final List<Sprite> effectSprites;
+	private Sprite playerSprite;
 	
 	public PlayerHudImpl() {
 		name = "";
 		score = "";
 		alive = true;
+		effectSprites = new ArrayList<>();
 	}
 	
 	@Override
@@ -42,6 +49,21 @@ public class PlayerHudImpl implements PlayerHud {
 	@Override
 	public void setAlive(boolean alive) {
 		this.alive = alive;
+	}
+
+	@Override
+	public void setSnakeSprite(Sprite sprite) {
+		playerSprite = sprite;
+	}
+
+	@Override
+	public void addEffectSprite(Sprite sprite) {
+		effectSprites.add(sprite);
+	}
+
+	@Override
+	public void resetEffectSpriteList() {
+		effectSprites.clear();
 	}
 
 }
