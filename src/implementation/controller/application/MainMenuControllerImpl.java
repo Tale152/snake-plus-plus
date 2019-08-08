41776bacb6e751f76efefaf20925dbc0acffe79a
Elisa Tronetti <elisa.tronetti@studio.unibo.it>
2019-08-08 16:02:52 +0200

    Fixing the pack selection

--- src/implementation/controller/application/MainMenuControllerImpl.java
+++ src/implementation/controller/application/MainMenuControllerImpl.java
@@ -1,23 +1,28 @@
 package implementation.controller.application;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
+import design.controller.application.ClassicController;
 import design.controller.application.MainMenuController;
-import design.controller.game.GameLoader;
-import implementation.controller.game.gameLoader.GameLoaderJSON;
+import design.view.game.Sprite;
 import implementation.view.application.Main;
-import implementation.view.game.GameViewImpl;
+import implementation.view.game.SpriteImpl;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Labeled;
+import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 
 public class MainMenuControllerImpl implements MainMenuController, Initializable {
@@ -27,26 +32,19 @@ public class MainMenuControllerImpl implements MainMenuController, Initializable
 	@FXML private Button level;
 	@FXML private Label snakeppLabel;
 	@FXML private AnchorPane root;
-	@FXML private MenuItem pack1;
-	@FXML private MenuItem defaultPack;
+	@FXML private MenuButton skinPacks;
 	
 	//default skin pack that can be change
 	private String skinPackPath;
+	private final ArrayList<String> itemButtonList = new ArrayList<>();
 	
 	@FXML
 	public void goToClassicMode() throws IOException {
 		new SettingsControllerImpl();
-		//funziona solo così, con isEmpty va in nullPointer, idk
-		if(this.skinPackPath == null) {
-			selectDefaultSkinPack();
-		}
-		List<String> playerNames = new ArrayList<>(Arrays.asList("Ale"));
-		String levelPath = "res/stages/classic/HightPoints5MinMedium.json";
-		GameLoader gl;
-		gl = new GameLoaderJSON(levelPath, playerNames);
-		@SuppressWarnings("unused")
-		GameViewImpl gw = new GameViewImpl(Main.getScene(), levelPath, this.skinPackPath, playerNames, 
-	    		gl.getGameModel().getField().getWidth(), gl.getGameModel().getField().getHeight());
+		FXMLLoader root = new FXMLLoader(getClass().getResource("/implementation/view/application/ClassicView.fxml"));
+		Main.getScene().setRoot(root.load());
+		ClassicController controller = root.getController();
+		controller.setSkinPackPath(skinPackPath);
 	}
 	
 	@FXML
@@ -54,20 +52,31 @@ public class MainMenuControllerImpl implements MainMenuController, Initializable
 		
 	}
 	
-	@FXML
-	public void selectDefaultSkinPack(){
-		String defaultName = this.defaultPack.getText();
-		this.skinPackPath = "res" + File.separator + "resources" + File.separator + defaultName.replaceAll("\\s", "");
-	}
+//	@FXML
+//	public void selectDefaultSkinPack(){
+//		String defaultName = this.defaultPack.getText();
+//		this.skinPackPath = "res" + File.separator + "resources" + File.separator + defaultName.replaceAll("\\s", "");
+//	}
+//	
+//	@FXML
+//	public void selectSkinPack1(){
+//		String pack1Name = this.pack1.getText();
+//		this.skinPackPath = "res" + File.separator + "resources" + File.separator + pack1Name.replaceAll("\\s", "");
+//	}
 	
-	@FXML
-	public void selectSkinPack1(){
-		String pack1Name = this.pack1.getText();
-		this.skinPackPath = "res" + File.separator + "resources" + File.separator + pack1Name.replaceAll("\\s", "");
-	}
+	
 	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		final File folder = new File("res" + File.separator + "resources");
+		listFiles(folder);
+		
+		
+		for(String s : this.itemButtonList) {
+			this.skinPacks.getItems().add(new MenuItem(s));
+		}
+		
 		root.heightProperty().addListener(new ChangeListener<Object>() {
 
 			@Override
@@ -78,7 +87,17 @@ public class MainMenuControllerImpl implements MainMenuController, Initializable
 			}
 			
 		});
+		
+//		selectDefaultSkinPack();
 
+	}
+	
+	private void listFiles(final File folder) {
+	    for (final File fileEntry : folder.listFiles()) {
+	        if (fileEntry.isDirectory()) {
+	        	this.itemButtonList.add(fileEntry.getName().replace("_", " "));
+	        }
+	    }
 	}
 	
 	private void changeFontSize(Labeled labeled, double percentage) {
