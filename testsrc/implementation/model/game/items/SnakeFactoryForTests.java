d487e1c60b8708c426baa6a1f6217760b24def14
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-08 16:15:41 +0200

    fixed SnakeFactoryForTests and AppleTest

--- testsrc/implementation/model/game/items/SnakeFactoryForTests.java
+++ testsrc/implementation/model/game/items/SnakeFactoryForTests.java
@@ -8,25 +8,25 @@ import implementation.model.game.snake.SnakeImpl;
 
 public class SnakeFactoryForTests {
 
-	public static Snake baseSnake(List<Point> points) {
-		return new SnakeImpl(points, PlayerNumber.PLAYER1, "Player", Direction.RIGHT, 1000L, 1.0, 0L);
+	public static Snake baseSnake(List<Point> points, Field field) {
+		return new SnakeImpl(PlayerNumber.PLAYER1, "Player", Direction.RIGHT, 1000L, 1.0, field,points);
 	}
 	
-	public static Snake ghostSnake(List<Point> points) {
-		Snake snake = baseSnake(points);
-		snake.getProperties().getCollision().setIntangibility(true);
+	public static Snake ghostSnake(List<Point> points, Field field) {
+		Snake snake = baseSnake(points, field);
+		snake.getProperties().getCollisionProperty().setIntangibility(true);
 		return snake;
 	}
 	
-	public static Snake godSnake(List<Point> points) {
-		Snake snake = baseSnake(points);
-		snake.getProperties().getCollision().setInvincibility(true);
+	public static Snake godSnake(List<Point> points, Field field) {
+		Snake snake = baseSnake(points, field);
+		snake.getProperties().getCollisionProperty().setInvincibility(true);
 		return snake;
 	}
 	
-	public static Snake springSnake(List<Point> points) {
-		Snake snake = baseSnake(points);
-		snake.getProperties().getCollision().setSpring(true);
+	public static Snake springSnake(List<Point> points, Field field) {
+		Snake snake = baseSnake(points, field);
+		snake.getProperties().getCollisionProperty().setSpring(true);
 		return snake;
 	}
 	
