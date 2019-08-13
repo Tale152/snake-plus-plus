1dc360c816cc38f5c90c46a83e3b331383052b9e
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-13 13:58:58 +0200

    refactored and added javadoc to ScoreEarning

--- src/implementation/model/game/items/ScoreEarning.java
+++ src/implementation/model/game/items/ScoreEarning.java
@@ -6,36 +6,52 @@ import design.model.game.Effect;
 import design.model.game.Field;
 import design.model.game.Snake;
 
-public class ScoreEarning extends EffectAbstract{
+/**
+ * Instant effect: increases snake's score by double of Apple's score increment.<p>
+ * Lasting effect: increases snake's score by a single ScoreEarning SCORE_INCREMENT value
+ * multiplied by number of ScoreEarning collided before lasting effect ends.
+ * @author Alessandro Talmi
+ */
+public class ScoreEarning extends EffectAbstract {
 
-	public ScoreEarning(Optional<Long> dEffectDuration) {
-		super(dEffectDuration);
-	}
+    /**
+     * How much a snake's score will be increased.
+     */
+    public static final int SCORE_INCREMENT = Apple.SCORE_INCREMENT * 2;
 
-	@Override
-	public void instantaneousEffect(Snake target) {
-		Optional<Effect> active = target.getEffects().stream().filter(e -> {return e.getClass().equals(this.getClass());}).findFirst();
-		if (active.isPresent()) {
-			target.getPlayer().addScore(Apple.SCORE_INCREMENT * (active.get().getComboCounter() + 1));
-		}
-		else {
-			target.getPlayer().addScore(Apple.SCORE_INCREMENT);
-		}
-	}
+    /**
+     * @param dEffectDuration how long this effect's lasting effect will last,
+     * empty if no effect duration
+     */
+    public ScoreEarning(final Optional<Long> dEffectDuration) {
+        super(dEffectDuration);
+    }
 
-	@Override
-	public void expirationEffect(Field field) {
-		// does nothing
-	}
+    @Override
+    public final void instantaneousEffect(final Snake target) {
+        Optional<Effect> active = target.getEffects().stream().filter(e -> {
+            return e.getClass().equals(this.getClass()); })
+                .findFirst();
+        if (active.isPresent()) {
+            target.getPlayer().addScore(SCORE_INCREMENT * (active.get().getComboCounter() + 1));
+        } else {
+            target.getPlayer().addScore(SCORE_INCREMENT);
+        }
+    }
 
-	@Override
-	protected void behaviorOnLastingEffectStart(Snake snake) {
-		// does nothing
-	}
+    @Override
+    public final void expirationEffect(final Field field) {
+        // does nothing
+    }
 
-	@Override
-	protected void behaviorOnLastingEffectEnd(Snake snake) {
-		// does nothing
-	}
-	
+    @Override
+    protected void behaviorOnLastingEffectStart(final Snake snake) {
+        // does nothing
+    }
+
+    @Override
+    protected void behaviorOnLastingEffectEnd(final Snake snake) {
+        // does nothing
+    }
+
 }
