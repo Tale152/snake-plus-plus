eb166705f153a6fd57eb40d42e5d843786e64838
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-13 14:21:05 +0200

    refactored and added javadoc to ItemFactory

--- src/implementation/model/game/items/ItemFactory.java
+++ src/implementation/model/game/items/ItemFactory.java
@@ -2,20 +2,41 @@ package implementation.model.game.items;
 
 import java.awt.Point;
 import java.util.Optional;
-
 import design.model.game.Effect;
 import design.model.game.Field;
 import design.model.game.Item;
 
+/**
+ * Allows to create items with any effect that defines them.<p>
+ * Having the effect class into arguments allows automates the process
+ * of spawning items.
+ * @author Alessandro Talmi
+ */
 public class ItemFactory {
-	private final Field field;
 
-	public ItemFactory(Field field) {
-		this.field = field;
-	}
-	
-	public Item createItem(Point point, Class<? extends Effect> effectClass, Optional<Long> dExpire, Optional<Long> dEffectDuration) {
-		return new ItemImpl(field, point, effectClass, dExpire, dEffectDuration);
-	}
-	
+    private final Field field;
+
+    /**
+     Instantiate a new ItemFactory.
+     * @param field where created items will be placed
+     */
+    public ItemFactory(final Field field) {
+        this.field = field;
+    }
+
+    /**
+     * Creates an Item with specified features.
+     * @param point coordinates in the field
+     * @param effectClass that determinate item behavior
+     * @param dExpire delta (milliseconds) before item disappears from field
+     * (empty if you want it to not disappear)
+     * @param dEffectDuration delta (milliseconds) before item's lasting effect ends
+     * (empty if you want it to not activate lasting effect)
+     * @return desired item
+     */
+    public final Item createItem(final Point point, final Class<? extends Effect> effectClass,
+            final Optional<Long> dExpire, final Optional<Long> dEffectDuration) {
+        return new ItemImpl(field, point, effectClass, dExpire, dEffectDuration);
+    }
+
 }
