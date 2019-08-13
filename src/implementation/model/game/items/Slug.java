7ff019e0e190ea0b4827535d0f85bc9fe25a3333
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-13 14:07:18 +0200

    refactored and added javadoc to Slug

--- src/implementation/model/game/items/Slug.java
+++ src/implementation/model/game/items/Slug.java
@@ -1,38 +1,46 @@
 package implementation.model.game.items;
 
 import java.util.Optional;
-
 import design.model.game.Field;
 import design.model.game.Snake;
 
-public class Slug extends EffectAbstract{
+/**
+ * Instant effect: nothing. <p>
+ * Lasting effect: halves snake's speed.
+ * @author Alessandro Talmi
+ */
+public class Slug extends EffectAbstract {
 
-	private double delta;
-	
-	public Slug(Optional<Long> dEffectDuration) {
-		super(dEffectDuration);
-		delta = 0;
-	}
+    private double delta;
 
-	@Override
-	public void instantaneousEffect(Snake target) {
-		//does nothing
-	}
+    /**
+     * @param dEffectDuration how long this effect's lasting effect will last,
+     * empty if no effect duration
+     */
+    public Slug(final Optional<Long> dEffectDuration) {
+        super(dEffectDuration);
+        delta = 0;
+    }
 
-	@Override
-	public void expirationEffect(Field field) {
-		//does nothing
-	}
+    @Override
+    public void instantaneousEffect(final Snake target) {
+        //does nothing
+    }
 
-	@Override
-	protected void behaviorOnLastingEffectStart(Snake snake) {
-		delta = (snake.getProperties().getSpeedProperty().getSpeedMultiplier() * Turbo.SPEED_MULTIPLICATOR) - snake.getProperties().getSpeedProperty().getSpeedMultiplier();
-		snake.getProperties().getSpeedProperty().applySpeedMultiplier(delta);
-	}
+    @Override
+    public void expirationEffect(final Field field) {
+        //does nothing
+    }
 
-	@Override
-	protected void behaviorOnLastingEffectEnd(Snake snake) {
-		snake.getProperties().getSpeedProperty().applySpeedMultiplier(-delta);
-	}
+    @Override
+    protected final void behaviorOnLastingEffectStart(final Snake snake) {
+        delta = (snake.getProperties().getSpeedProperty().getSpeedMultiplier() * Turbo.SPEED_MULTIPLICATOR) - snake.getProperties().getSpeedProperty().getSpeedMultiplier();
+        snake.getProperties().getSpeedProperty().applySpeedMultiplier(delta);
+    }
+
+    @Override
+    protected final void behaviorOnLastingEffectEnd(final Snake snake) {
+        snake.getProperties().getSpeedProperty().applySpeedMultiplier(-delta);
+    }
 
 }
