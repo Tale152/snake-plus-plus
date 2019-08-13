de254e6961bbc69782490bb0f18a11c833115037
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-13 11:12:08 +0200

    refactored and added javadoc to Collidable interface

--- src/design/model/game/Collidable.java
+++ src/design/model/game/Collidable.java
@@ -3,12 +3,38 @@ package design.model.game;
 import java.awt.Point;
 import java.lang.reflect.InvocationTargetException;
 
+/**
+*A collidable is the root of everything that stands in a cell into the field and that you can collide with.
+*
+* @author Alessandro Talmi
+* @author Elisa Tronetti
+* @author Nicola Orlando
+*
+*/
 public interface Collidable {
 
-	public void onCollision(Snake collider) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException;
-	
-	public Point getPoint();
-	
-	public void setPoint(Point point);
-	
+    /**
+    * Specifies what happens when a snake collides with this collidable.
+    * @param collider the snake that collides with this collidable
+    * @throws NoSuchMethodException la
+    * @throws SecurityException la
+    * @throws InstantiationException la
+    * @throws IllegalAccessException la
+    * @throws IllegalArgumentException la
+    * @throws InvocationTargetException la
+    */
+    void onCollision(Snake collider) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException;
+
+    /**
+    * Return the current point representing the coordinates of this collidable into the field.
+    * @return coordinates of this collidable
+    */
+    Point getPoint();
+
+    /**
+    * Sets a new Point representing the coordinates of this collidable into the field.
+    * @param point representing where this collidable now stands into the field
+    */
+    void setPoint(Point point);
+
 }
