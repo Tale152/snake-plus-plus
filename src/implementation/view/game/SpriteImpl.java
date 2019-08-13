18c88286cdcd0a5cfe4c26e636df824db40c816e
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-13 17:44:55 +0200

    refactored and added javadoc to SpriteImpl

--- src/implementation/view/game/SpriteImpl.java
+++ src/implementation/view/game/SpriteImpl.java
@@ -3,23 +3,31 @@ package implementation.view.game;
 import design.view.game.Sprite;
 import javafx.scene.image.Image;
 
+/**
+ * @see Sprite
+ * @author Alessandro Talmi
+ */
 public class SpriteImpl implements Sprite {
 
-	private final String name;
-	private final Image sprite;
-	
-	public SpriteImpl(String name, Image sprite) {
-		this.name = name;
-		this.sprite = sprite;
-	}
-	
-	@Override
-	public String getName() {
-		return name;
-	}
+    private final String name;
+    private final Image sprite;
 
-	@Override
-	public Image getSprite() {
-		return sprite;
-	}
+    /**
+     * @param name of the sprite
+     * @param sprite the actual sprite Image
+     */
+    public SpriteImpl(final String name, final Image sprite) {
+        this.name = name;
+        this.sprite = sprite;
+    }
+
+    @Override
+    public final String getName() {
+        return name;
+    }
+
+    @Override
+    public final Image getSprite() {
+        return sprite;
+    }
 }
