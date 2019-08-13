8c7127dda1599d2374732e4318a6d3c529cc933d
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-13 17:48:31 +0200

    refactored and added javadoc to PlayerHudImpl

--- src/implementation/view/game/PlayerHudImpl.java
+++ src/implementation/view/game/PlayerHudImpl.java
@@ -6,82 +6,89 @@ import java.util.List;
 import design.view.game.PlayerHud;
 import design.view.game.Sprite;
 
+/**
+ * @see PlayerHud
+ * @author Alessandro Talmi 
+ */
 public class PlayerHudImpl implements PlayerHud {
 
-	private String name;
-	private String score;
-	private boolean alive;
-	private final List<Sprite> actualSprites;
-	private final List<Sprite> currentSprites;
-	private Sprite playerSprite;
-	
-	public PlayerHudImpl() {
-		name = "";
-		score = "";
-		alive = true;
-		actualSprites = new ArrayList<>();
-		currentSprites = new ArrayList<>();
-	}
-	
-	@Override
-	public String getName() {
-		return name;
-	}
+    private String name;
+    private String score;
+    private boolean alive;
+    private final List<Sprite> actualSprites;
+    private final List<Sprite> currentSprites;
+    private Sprite playerSprite;
 
-	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * PlayerHudImpl's constructor.
+     */
+    public PlayerHudImpl() {
+        name = "";
+        score = "";
+        alive = true;
+        actualSprites = new ArrayList<>();
+        currentSprites = new ArrayList<>();
+    }
 
-	@Override
-	public String getScore() {
-		return score;
-	}
+    @Override
+    public final String getName() {
+        return name;
+    }
 
-	@Override
-	public void setScore(String score) {
-		this.score = score;
-	}
+    @Override
+    public final void setName(final String name) {
+        this.name = name;
+    }
 
-	@Override
-	public boolean isAlive() {
-		return alive;
-	}
+    @Override
+    public final String getScore() {
+        return score;
+    }
 
-	@Override
-	public void setAlive(boolean alive) {
-		this.alive = alive;
-	}
+    @Override
+    public final void setScore(final String score) {
+        this.score = score;
+    }
 
-	@Override
-	public void setSnakeSprite(Sprite sprite) {
-		playerSprite = sprite;
-	}
+    @Override
+    public final boolean isAlive() {
+        return alive;
+    }
 
-	@Override
-	public void addEffectSprite(Sprite sprite) {
-		currentSprites.add(sprite);
-	}
+    @Override
+    public final void setAlive(final boolean alive) {
+        this.alive = alive;
+    }
 
-	@Override
-	public List<Sprite> getSpriteList() {
-		List<Sprite> res = new ArrayList<Sprite>();
-		if (playerSprite != null) {
-			res.add(playerSprite);
-		}
-		res.addAll(actualSprites);
-		return res;
-	}
+    @Override
+    public final void setSnakeSprite(final Sprite sprite) {
+        playerSprite = sprite;
+    }
 
-	@Override
-	public void newEffectSpriteList() {
-		currentSprites.clear();
-	}
+    @Override
+    public final void addEffectSprite(final Sprite sprite) {
+        currentSprites.add(sprite);
+    }
 
-	@Override
-	public void endEffectSpriteList() {
-		actualSprites.clear();
-		actualSprites.addAll(currentSprites);
-	}
+    @Override
+    public final List<Sprite> getSpriteList() {
+        List<Sprite> res = new ArrayList<Sprite>();
+        if (playerSprite != null) {
+            res.add(playerSprite);
+        }
+        res.addAll(actualSprites);
+        return res;
+    }
+
+    @Override
+    public final void newEffectSpriteList() {
+        currentSprites.clear();
+    }
+
+    @Override
+    public final void endEffectSpriteList() {
+        actualSprites.clear();
+        actualSprites.addAll(currentSprites);
+    }
 
 }
