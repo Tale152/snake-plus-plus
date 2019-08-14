9d8c3c3c90afee12e0240796f6ae0fb6c1b6089a
Elisa Tronetti <elisa.tronetti@studio.unibo.it>
2019-08-14 15:13:06 +0200

    Implementing new description menu

--- src/implementation/controller/application/DescriptionControllerImpl.java
+++ src/implementation/controller/application/DescriptionControllerImpl.java
@@ -1,8 +1,10 @@
 package implementation.controller.application;
 
 
+import java.awt.Button;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -11,26 +13,26 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.Labeled;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
 public class DescriptionControllerImpl implements Initializable {
 
   private final static String PATH = "res" + File.separator + "descriptions" + File.separator;
+  private final static String PATH_ITEMS = "res" + File.separator + "resources" + File.separator + "Default_Pack" + File.separator + "Items" + File.separator;
   
   @FXML private MenuButton selectItem;
   @FXML private Label itemDescription;
-  @FXML private AnchorPane root;
-  
+  @FXML private AnchorPane imageSpot;
+
   private final Map<String, String> descriptionButtonMap = new HashMap<>();
   String labelStr;
   
@@ -39,6 +41,7 @@ public class DescriptionControllerImpl implements Initializable {
     final File folder = new File("res" + File.separator + "descriptions");
     listFiles(folder);
     initializeMenuItem();
+    this.itemDescription.setWrapText(true);
   }
 
   private void listFiles(final File folder) {
@@ -73,6 +76,21 @@ public class DescriptionControllerImpl implements Initializable {
                   
                 selectItem.setText(m.getText());
                 itemDescription.setText(labelStr);
+                
+                try {
+                  FileInputStream inputStream = new FileInputStream(PATH_ITEMS + m.getText().replace(" ", "") + ".png");
+                  Image item = new Image(inputStream);
+                  ImageView itemImage = new ImageView(item);
+                  imageSpot.getChildren().clear();
+                  imageSpot.getChildren().add(itemImage);
+                  itemImage.setPreserveRatio(true);
+                 
+     
+
+                } catch (FileNotFoundException e1) {
+                  // TODO Auto-generated catch block
+                  e1.printStackTrace();
+                }                
               } 
           }; 
       m.setOnAction(event);
