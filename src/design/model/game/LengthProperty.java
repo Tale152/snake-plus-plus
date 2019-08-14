d646f13549c0ecc3455a330a3f7ca464d8809790
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-14 16:46:22 +0200

    refactored and added javadoc to LengthProperty and SpeedProperty

--- src/design/model/game/LengthProperty.java
+++ src/design/model/game/LengthProperty.java
@@ -1,11 +1,29 @@
 package design.model.game;
 
+/**
+ * A set of method for managing snake's length. <p>
+ * Here snake's length is intended like a logical size.
+ * For instance on screen a snake can be long 3 but in here is 5, that means that
+ * in 2 movements the actual size of the snake will reach it's logical size.
+ * @author Alessandro Talmi
+ * @author Elisa Tronetti
+ * @author Nicola Orlando
+ */
 public interface LengthProperty {
 
+    /**
+     * @return get current logical snake's length
+     */
     int getLength();
 
+    /**
+     * @param n how much to increase snake's length
+     */
     void lengthen(int n);
 
+    /**
+     * @param n how much to decrease snake's length
+     */
     void shorten(int n);
 
 }
--- src/design/model/game/SpeedProperty.java
+++ src/design/model/game/SpeedProperty.java
@@ -1,17 +1,41 @@
 package design.model.game;
 
+/**
+ * A set of properties useful to handle snake's speed.
+ * @author Alessandro Talmi
+ * @author Elisa Tronetti
+ * @author Nicola Orlando
+ */
 public interface SpeedProperty {
 
+    /**
+     * @return the delta (milliseconds) between two snake movements
+     */
     long getDeltaT();
 
+    /**
+     * @param deltaT (milliseconds) representing the time between two snake movements
+     */
     void setDeltaT(long deltaT);
 
+    /**
+     * @param mul multiplier that will be added to the current speed multiplier
+     */
     void applySpeedMultiplier(double mul);
 
+    /**
+     * @return the speed multiplier that will be applied to snake's deltaT
+     */
     double getSpeedMultiplier();
 
+    /**
+     * @param val the value that will be applied to delta to determinate snake's speed based on length
+     */
     void applyLenghtSpeedValue(double val);
 
+    /**
+     * @return the value that will be applied to delta to determinate snake's speed based on length
+     */
     double getLenghtSpeedValue();
 
 }
