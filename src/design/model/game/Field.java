33b37b6ddc21e348d368fc94963bffe95a167cec
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-14 15:12:48 +0200

    refactored and added javadoc to Wall, WinConditions, LossConditions and ItemRule

--- src/design/model/game/Field.java
+++ src/design/model/game/Field.java
@@ -4,37 +4,37 @@ import java.awt.Point;
 import java.util.List;
 
 public interface Field {
-	
-	public void begin();
-	
-	public List<Item> getEliminatedItems();
-	
-	public int getWidth();
 
-	public int getHeight();
-	
-	public List<Collidable> getCell(Point point);
-	
-	public List<Item> getItems();
+    void begin();
 
-	public boolean addItem(Item item) throws IllegalStateException;
-	
-	public boolean removeItem(Item item);
-	
-	public List<Wall> getWalls();
-	
-	public boolean addWall(Wall wall) throws IllegalStateException;
-	
-	public List<BodyPart> getBodyParts();
-	
-	public boolean addBodyPart(BodyPart bodyPart) throws IllegalStateException;
-	
-	public boolean removeBodyPart(BodyPart bodyPart);
-	
-	public List<Snake> getSnakes();
-	
-	public boolean addSnake(Snake snake);
-	
-	public Snake removeSnake(int i);
-	
+    List<Item> getEliminatedItems();
+
+    int getWidth();
+
+    int getHeight();
+
+    List<Collidable> getCell(Point point);
+
+    List<Item> getItems();
+
+    boolean addItem(Item item) throws IllegalStateException;
+
+    boolean removeItem(Item item);
+
+    List<Wall> getWalls();
+
+    boolean addWall(Wall wall) throws IllegalStateException;
+
+    List<BodyPart> getBodyParts();
+
+    boolean addBodyPart(BodyPart bodyPart) throws IllegalStateException;
+
+    boolean removeBodyPart(BodyPart bodyPart);
+
+    List<Snake> getSnakes();
+
+    boolean addSnake(Snake snake);
+
+    Snake removeSnake(int i);
+
 }
--- src/design/model/game/LossConditions.java
+++ src/design/model/game/LossConditions.java
@@ -2,10 +2,24 @@ package design.model.game;
 
 import java.util.List;
 
+/**
+ * The conditions to loose a game. Not necessarily all conditions are active.
+ * @author Alessandro Talmi
+ * @author Elisa Tronetti
+ * @author Nicola Orlando
+ */
 public interface LossConditions {
-	
-	public boolean checkSnakes(List<Snake> snakes);
-	
-	public boolean checkTime(Long time);
+
+    /**
+     * @param snakes list of every snake into the field
+     * @return whether or not you have loose because all snake died
+     */
+    boolean checkSnakes(List<Snake> snakes);
+
+    /**
+     * @param time current game time (milliseconds)
+     * @return whether or not you have loose because certain time is reached
+     */
+    boolean checkTime(Long time);
 
 }
