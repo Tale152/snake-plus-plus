74cf0a26996671c5fd59f38c4d2065a44f80540e
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-14 14:52:23 +0200

    refactored and added javadoc to MainMenuController, GameModel, GameRules

--- src/design/controller/application/MainMenuController.java
+++ src/design/controller/application/MainMenuController.java
@@ -2,10 +2,24 @@ package design.controller.application;
 
 import java.io.IOException;
 
+/**
+ * The controller of the Main Menu, containing methods to access all the game modes.
+ * @author Alessandro Talmi
+ * @author Elisa Tronetti
+ * @author Nicola Orlando
+ */
 public interface MainMenuController {
 
-	public void goToClassicMode() throws IOException;
-	
-	public void goToLevelMode();
-	
+    /**
+     * Loads the Classic Mode menu.
+     * @throws IOException if levels not found
+     */
+    void goToClassicMode() throws IOException;
+
+    /**
+     * Loads the Level Mode menu.
+     * @throws IOException if levels not found
+     */
+    void goToLevelMode() throws IOException;
+
 }
