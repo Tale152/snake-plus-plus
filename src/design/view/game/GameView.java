67dbb70f4d8ba9deedb9c025acbefa9bb3ed5732
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-15 17:16:47 +0200

    refactored and add javadoc to GameView interface

--- src/design/view/game/GameView.java
+++ src/design/view/game/GameView.java
@@ -2,16 +2,40 @@ package design.view.game;
 
 import design.controller.game.GameController;
 
+/**
+ * Contains everything needed to display a game.
+ * @see GameHud
+ * @see GameField
+ * @see GameController
+ * @author Alessandro Talmi
+ * @author Elisa Tronetti
+ * @author Nicola Orlando
+ */
 public interface GameView {
 
+    /**
+     * @return the game's hud
+     */
     GameHud getHUD();
 
+    /**
+     * @return the graphical game's field
+     */
     GameField getField();
 
+    /**
+     * Starts printing.
+     */
     void startRendering();
 
+    /**
+     * Stops printing.
+     */
     void stopRendering();
 
+    /**
+     * @return the game controller controlling this level
+     */
     GameController getGameController();
 
 }
