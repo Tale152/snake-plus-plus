c04d035ca405348c0ebab6ea72ce26b578d2978f
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-15 17:24:16 +0200

    partially refactored and added javadoc to CollisionPropertyImpl

--- src/implementation/model/game/snake/CollisionPropertyImpl.java
+++ src/implementation/model/game/snake/CollisionPropertyImpl.java
@@ -2,52 +2,59 @@ package implementation.model.game.snake;
 
 import design.model.game.CollisionProperty;
 
+/**
+ * BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
+ * BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
+ * BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA BLA
+ * @see CollisionProperty
+ * @author Elisa Tronetti
+ */
 public class CollisionPropertyImpl implements CollisionProperty{
 
-	private boolean invincible;
-	private boolean intangible;
-	private boolean spring;
-	
-	public CollisionPropertyImpl() {
-		this.invincible = false;
-		this.intangible = false;
-		this.spring = false;
-	}
-	
-	@Override
-	public void setInvincibility(boolean inv) {
-		this.invincible = inv;	
-	}
+    private boolean invincible;
+    private boolean intangible;
+    private boolean spring;
 
-	@Override
-	public boolean getInvincibility() {
-		return this.invincible;
-	}
+    public CollisionPropertyImpl() {
+        this.invincible = false;
+        this.intangible = false;
+        this.spring = false;
+    }
 
-	@Override
-	public void setIntangibility(boolean intangibility) {
-		this.intangible = intangibility;
-	}
+    @Override
+    public final void setInvincibility(final boolean inv) {
+        this.invincible = inv;
+    }
 
-	@Override
-	public boolean getIntangibility() {
-		return this.intangible;
-	}
+    @Override
+    public final boolean getInvincibility() {
+        return this.invincible;
+    }
 
-	@Override
-	public void setSpring(boolean spring) {
-		this.spring = spring;
-	}
+    @Override
+    public final void setIntangibility(final boolean intangibility) {
+        this.intangible = intangibility;
+    }
 
-	@Override
-	public boolean getSpring() {
-		return this.spring;
-	}
-	
-	public String toString() {
-		return "Invincibility: " + this.invincible + "\n"
-				+ "Intangibility: " + this.intangible + "\n"
-				+ "Spring: " + this.spring + "\n";
-	}
+    @Override
+    public final boolean getIntangibility() {
+        return this.intangible;
+    }
+
+    @Override
+    public final void setSpring(final boolean spring) {
+        this.spring = spring;
+    }
+
+    @Override
+    public final boolean getSpring() {
+        return this.spring;
+    }
+
+    public String toString() {
+        return "Invincibility: " + this.invincible + "\n"
+                + "Intangibility: " + this.intangible + "\n"
+                + "Spring: " + this.spring + "\n";
+    }
 
 }
