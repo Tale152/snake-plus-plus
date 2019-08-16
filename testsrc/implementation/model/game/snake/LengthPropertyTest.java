c270545d891acb660afd10744a595ccbf695e5ad
Elisa Tronetti <elisa.tronetti@studio.unibo.it>
2019-08-16 17:52:19 +0200

    Fixed almost every warning in length property test

--- testsrc/implementation/model/game/snake/LengthPropertyTest.java
+++ testsrc/implementation/model/game/snake/LengthPropertyTest.java
@@ -4,41 +4,50 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 import design.model.game.LengthProperty;
-
+/**
+ * Tests regarding snake length properties.
+ * @see LengthProperty
+ */
 public class LengthPropertyTest {
-	
-	@Test
-	public void testLengthProperty() {
-		LengthProperty length = SnakeComponentsFactoryForTest.createLengthProperty();
-		assertEquals(length.getLength(), 1);
-		length.lengthen(4);
-		assertEquals(length.getLength(), 5);
-		length.lengthen(1);
-		assertEquals(length.getLength(), 6);
-		length.lengthen(0);
-		assertEquals(length.getLength(), 6);
-		try{
-			length.lengthen(-1);
+
+    private static final int FIVE_LENGTH = 5;
+    private static final int SIX_LENGTH = 6;
+
+    /**
+     * Test if snake length is correct and if his length is shorten or increased correctly.
+     */
+    @Test
+    public void testLengthProperty() {
+        final LengthProperty length = SnakeComponentsFactoryForTest.createLengthProperty();
+        assertEquals("Check if snake current length is 1", length.getLength(), 1);
+        length.lengthen(4);
+        assertEquals("Check if snake current length is 5, after lengthen by 4", length.getLength(), FIVE_LENGTH);
+        length.lengthen(1);
+        assertEquals("Check if snake current length is 6, after lengthen by 1", length.getLength(), SIX_LENGTH);
+        length.lengthen(0);
+        assertEquals("Check if snake current length is 6, after lengthen by 0", length.getLength(), SIX_LENGTH);
+        try {
+            length.lengthen(-1);
             fail("lengthen arg cannot be negative");
-        } catch (IllegalArgumentException e){
-        } catch (Exception e){
+        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
             fail("wrong exception");
         }
-		length.shorten(0);
-		assertEquals(length.getLength(), 6);
-		try{
-			length.shorten(-1);
+        length.shorten(0);
+        assertEquals("Check if snake current length is 6 aftern shorten by 0", length.getLength(), SIX_LENGTH);
+        try {
+            length.shorten(-1);
             fail("shorten arg cannot be negative");
-        } catch (IllegalArgumentException e){
-        } catch (Exception e){
+        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
             fail("wrong exception");
         }
-		length.shorten(1);
-		assertEquals(length.getLength(), 5);
-		length.shorten(3);
-		assertEquals(length.getLength(), 2);
-		length.shorten(3);
-		assertEquals(length.getLength(), 1);
-		
-	}
+        length.shorten(1);
+        assertEquals("Check if snake current length is 5, after shorten by 1", length.getLength(), FIVE_LENGTH);
+        length.shorten(3);
+        assertEquals("Check if snake current length is 2, after shorten by 3", length.getLength(), 2);
+        length.shorten(3);
+        assertEquals("Check if snake current length is 1, after shorten by 3", length.getLength(), 1);
+    }
+
 }
