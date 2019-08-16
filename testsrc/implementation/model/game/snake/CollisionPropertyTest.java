5da7bc26625b1b7964bf1a02cadf205bf51f9632
Elisa Tronetti <elisa.tronetti@studio.unibo.it>
2019-08-16 16:07:06 +0200

    Fixed style in collision property test

--- testsrc/implementation/model/game/snake/CollisionPropertyTest.java
+++ testsrc/implementation/model/game/snake/CollisionPropertyTest.java
@@ -1,27 +1,36 @@
 package implementation.model.game.snake;
 
-import static org.junit.Assert.*;
-import org.junit.Test;
-import design.model.game.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
+import design.model.game.CollisionProperty;
+
+/**
+ * Tests regarding snake collision properties.
+ * @see CollisionProperty
+ */
 public class CollisionPropertyTest {
-	
-	@Test
-	public void testCollisionProperty() {
-		CollisionProperty collision = SnakeComponentsFactoryForTest.createCollisionProperty();
-		
-		assertFalse(collision.getInvincibility());
-		collision.setInvincibility(true);
-		assertTrue(collision.getInvincibility());
-		
-		assertFalse(collision.getIntangibility());
-		collision.setIntangibility(true);
-		assertTrue(collision.getIntangibility());
-		
-		assertFalse(collision.getSpring());
-		collision.setSpring(true);
-		assertTrue(collision.getSpring());
-		
-	}
+
+    /**
+     * Testing if snake invincibility, intangibiliy and spring are set correctly.
+     */
+    @Test
+    public void testCollisionProperty() {
+        final CollisionProperty collision = SnakeComponentsFactoryForTest.createCollisionProperty();
+
+        assertFalse("Checking if the invincibily is not active", collision.isInvincible());
+        collision.setInvincibility(true);
+        assertTrue("Checking if the invincibily has been activated", collision.isInvincible());
+
+        assertFalse("Checking if the intagibility is not active", collision.isIntangible());
+        collision.setIntangibility(true);
+        assertTrue("Checking if the intangibility has been activated", collision.isIntangible());
+
+        assertFalse("Checking if the spring is not active", collision.isSpring());
+        collision.setSpring(true);
+        assertTrue("Checking if the spring has been activated", collision.isSpring());
+    }
 
 }
