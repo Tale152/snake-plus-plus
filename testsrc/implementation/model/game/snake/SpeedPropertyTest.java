4e05d66b92f39da371dcbccefb4b3a2198b5c55a
Elisa Tronetti <elisa.tronetti@studio.unibo.it>
2019-08-16 18:38:33 +0200

    Fixed almost every style warning in speed property test

--- testsrc/implementation/model/game/snake/SpeedPropertyTest.java
+++ testsrc/implementation/model/game/snake/SpeedPropertyTest.java
@@ -1,71 +1,80 @@
 package implementation.model.game.snake;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertSame;
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 import design.model.game.SpeedProperty;
 
+/**
+ * Tests regarding snake speed properties.
+ */
 public class SpeedPropertyTest {
 
-	@Test
-	public void testSpeedProperty() {
-		SpeedProperty speed;
-		try{
-			speed = SnakeComponentsFactoryForTest.createSpeedProperty(0, 1);
+    private static final long DELTA = 900L;
+    private static final double SPEED_MULTIPLIER = 1.5;
+    /**
+     * Test if snake speed is correctly initialize and if the speed can be increased
+     * and decreased correctly.
+     */
+    @Test
+    public void testSpeedProperty() {
+        SpeedProperty speed;
+        try {
+            speed = SnakeComponentsFactoryForTest.createSpeedProperty(0, 1);
             fail("delta cannot be zero while initializing");
-        } catch (IllegalArgumentException e){
-        } catch (Exception e){
+        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
             fail("wrong exception");
         }
-		try{
-			speed = SnakeComponentsFactoryForTest.createSpeedProperty(-1, 1);
+        try {
+            speed = SnakeComponentsFactoryForTest.createSpeedProperty(-1, 1);
             fail("delta cannot be negative while initializing");
-        } catch (IllegalArgumentException e){
-        } catch (Exception e){
+        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
             fail("wrong exception");
         }
-		try{
-			speed = SnakeComponentsFactoryForTest.createSpeedProperty(100, -1);
+        try {
+            speed = SnakeComponentsFactoryForTest.createSpeedProperty(100, -1);
             fail("multiplier cannot be negative while initializing");
-        } catch (IllegalArgumentException e){
-        } catch (Exception e){
+        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
             fail("wrong exception");
         }
-		
-		speed = SnakeComponentsFactoryForTest.createSpeedProperty(900L, 1);
-		assertEquals(speed.getDeltaT(), 900L);
-		speed.setDeltaT(1000L);
-		assertEquals(speed.getDeltaT(), 1000L);
-		try{
-			speed.setDeltaT(0L);
+
+        speed = SnakeComponentsFactoryForTest.createSpeedProperty(DELTA, 1);
+        assertEquals("Check if the speed multiplier is correct", speed.getDeltaT(), DELTA);
+        speed.setDeltaT(1000L);
+        assertEquals("Check if the speed multiplier is correct after setting it", speed.getDeltaT(), 1000L);
+        try {
+            speed.setDeltaT(0L);
             fail("delta cannot be zero");
-        } catch (IllegalArgumentException e){
-        } catch (Exception e){
+        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
             fail("wrong exception");
         }
-		try{
-			speed.setDeltaT(-1L);
+        try {
+            speed.setDeltaT(-1L);
             fail("delta cannot be negative");
-        } catch (IllegalArgumentException e){
-        } catch (Exception e){
+        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
             fail("wrong exception");
         }
-		
-		assertTrue(speed.getSpeedMultiplier() == 1);
-		speed.applySpeedMultiplier(0.5);
-		assertTrue(speed.getSpeedMultiplier() == 1.5);
-		speed.applySpeedMultiplier(-1.5);
-		assertTrue(speed.getSpeedMultiplier() == 0);
-		try{
-			speed.applySpeedMultiplier(-1.5);
+
+        assertSame("Check if the speed multiplier is 1", speed.getSpeedMultiplier(), 1);
+        speed.applySpeedMultiplier(0.5);
+        assertSame("Check if the speed multiplier has been changed correctly", speed.getSpeedMultiplier(), SPEED_MULTIPLIER);
+        speed.applySpeedMultiplier(-SPEED_MULTIPLIER);
+        assertSame("Check if the speed multiplier is 0 when attempted to go negative", speed.getSpeedMultiplier(), 0);
+        try {
+            speed.applySpeedMultiplier(-SPEED_MULTIPLIER);
             fail("multiplier cannot be negative");
-        } catch (IllegalStateException e){
-        } catch (Exception e){
+        } catch (IllegalStateException e) {
+        } catch (Exception e) {
             fail("wrong exception");
         }
-		
-	}
+    }
+
 }
