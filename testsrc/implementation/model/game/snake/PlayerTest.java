95f0f13d77bdc4684f70809f6c1fc9ea5a7e6f58
Elisa Tronetti <elisa.tronetti@studio.unibo.it>
2019-08-16 17:53:58 +0200

    Fixed almost every style warning in player test

--- testsrc/implementation/model/game/snake/PlayerTest.java
+++ testsrc/implementation/model/game/snake/PlayerTest.java
@@ -1,54 +1,63 @@
 package implementation.model.game.snake;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 import design.model.game.Player;
 import design.model.game.PlayerNumber;
 
+/**
+ * Tests regarding player information.
+ */
 public class PlayerTest {
-	
-	@Test
-	public void testPlayer() {
-		Player player;
-		
-		try{
-			player = SnakeComponentsFactoryForTest.createPlayer(null, "Ale");
+
+    private static final int SCORE_SUPPOSED = 300;
+    /**
+     * Test if the player information are set correctly and if the score works.
+     */
+    @Test
+    public void testPlayer() {
+        Player player;
+
+        try {
+            player = SnakeComponentsFactoryForTest.createPlayer(null, "Ale");
             fail("Player number cannot be null");
-        } catch (IllegalArgumentException e){
-        } catch (Exception e){
+        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
             fail("wrong exception");
         }
-		
-		try{
-			player = SnakeComponentsFactoryForTest.createPlayer(PlayerNumber.PLAYER1, null);
+
+        try {
+            player = SnakeComponentsFactoryForTest.createPlayer(PlayerNumber.PLAYER1, null);
             fail("Player name cannot be null");
-        } catch (IllegalArgumentException e){
-        } catch (Exception e){
+        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
             fail("wrong exception");
         }
-		
-		player = SnakeComponentsFactoryForTest.createPlayer(PlayerNumber.PLAYER1, "Ale");
-		
-		assertEquals(player.getPlayerNumber(), PlayerNumber.PLAYER1);
-		assertEquals(player.getName(), "Ale");
-		assertEquals(player.getScore(), 0);
-		assertTrue(player.getScoreMultiplier() == 1);
-		
-		player.addScore(100);
-		assertEquals(player.getScore(), 100);
-		player.applyScoreMultiplier(2);
-		assertTrue(player.getScoreMultiplier() == 2);
-		player.addScore(100);
-		assertEquals(player.getScore(), 300);
-		player.reduceScore(100);
-		assertEquals(player.getScore(), 100);
-		player.applyScoreMultiplier(1);
-		player.reduceScore(100);
-		assertEquals(player.getScore(), 0);
-		player.reduceScore(100);
-		assertEquals(player.getScore(), 0);
-		
-	}
+
+        player = SnakeComponentsFactoryForTest.createPlayer(PlayerNumber.PLAYER1, "Ale");
+
+        assertEquals("Check if the player number is the player1", player.getPlayerNumber(), PlayerNumber.PLAYER1);
+        assertEquals("Check if the player name is Ale", player.getName(), "Ale");
+        assertEquals("Check if the initial score is 0", player.getScore(), 0);
+        assertEquals("Check if the initial score multiplier is 1", player.getScoreMultiplier(), 1, 0);
+
+        player.addScore(100);
+        assertEquals("Check if the score is 100, after adding 100 points and the score multiplier is 1",
+                player.getScore(), 100);
+        player.applyScoreMultiplier(2);
+        assertEquals("Check if the score multiplier is 2", player.getScoreMultiplier(), 2, 0);
+        player.addScore(100);
+        assertEquals("Check if the score is 300, after adding 100 points, the score was 100 and the score multiplier is 2",
+                player.getScore(), SCORE_SUPPOSED);
+        player.reduceScore(100);
+        assertEquals("Check if the score is 100, after subtacting 100 points, the score was 300 and the score multiplier is 2", 
+                player.getScore(), 100);
+        player.applyScoreMultiplier(1);
+        player.reduceScore(100);
+        assertEquals("Checking if the score is 0", player.getScore(), 0);
+        player.reduceScore(100);
+        assertEquals("Check if the score is 0 when trying to go negative", player.getScore(), 0);
+    }
+
 }
