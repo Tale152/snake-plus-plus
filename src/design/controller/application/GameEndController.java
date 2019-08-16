c816698ce534347e5d0a221058a00d9ea53ac51f
Alessandro Talmi <alessandro.talmi@studio.unibo.it>
2019-08-16 11:38:12 +0200

    fixed pmd src.design.controller.application

--- src/design/controller/application/GameEndController.java
+++ src/design/controller/application/GameEndController.java
@@ -1,5 +1,15 @@
 package design.controller.application;
 
+/**
+ * The GameEndController has to appear to the player at the end of a game to tell him if he has 
+ * won/lost and why.
+ * @author Alessandro Talmi
+ * @author Elisa Tronetti
+ * @author Nicola Orlando
+ */
 public interface GameEndController {
-	public void setEndReason(GameEndReason reason);
+    /**
+     * @param reason why the game ended
+     */
+    void setEndReason(GameEndReason reason);
 }
--- src/design/controller/application/WorldSelectionController.java
+++ src/design/controller/application/WorldSelectionController.java
@@ -1,5 +1,15 @@
 package design.controller.application;
 
+/**
+ * The word selection controller handles method used into the word selection view. <p>
+ * Here you can select "word levels". In other words when a level ends if you win
+ * you can go to the next level until you finish the world.
+ * @see ClassicController
+ * @see GameView
+ * @author Alessandro Talmi
+ * @author Elisa Tronetti
+ * @author Nicola Orlando
+ */
 public interface WorldSelectionController {
 
 }
