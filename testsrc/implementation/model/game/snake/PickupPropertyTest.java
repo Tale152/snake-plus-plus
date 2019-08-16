3d90d401407bc3aba0cf5191497ffce64ded0768
Elisa Tronetti <elisa.tronetti@studio.unibo.it>
2019-08-16 17:52:58 +0200

    Fixed almost every style warning in pick up property test

--- testsrc/implementation/model/game/snake/PickupPropertyTest.java
+++ testsrc/implementation/model/game/snake/PickupPropertyTest.java
@@ -4,28 +4,33 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import org.junit.Test;
 import design.model.game.PickupProperty;
-
+/**
+ * Tests regarding snake pick up properties.
+ */
 public class PickupPropertyTest {
-	
-	@Test
-	public void testPickupProperty() {
-		PickupProperty pickup = SnakeComponentsFactoryForTest.createPickupProperty();
-		assertEquals(pickup.getPickupRadius(), 1);
-		pickup.setPickupRadius(2);
-		assertEquals(pickup.getPickupRadius(), 2);
-		try{
-			pickup.setPickupRadius(0);
+
+    /**
+     * Test that check if snake pick up radius is set correctly.
+     */
+    @Test
+    public void testPickupProperty() {
+        final PickupProperty pickup = SnakeComponentsFactoryForTest.createPickupProperty();
+        assertEquals("Check if snake basic pick up radius is 1", pickup.getPickupRadius(), 1);
+        pickup.setPickupRadius(2);
+        assertEquals("Check if snake pick up radius is 2", pickup.getPickupRadius(), 2);
+        try {
+            pickup.setPickupRadius(0);
             fail("pickup range cannot be zero");
-        } catch (IllegalArgumentException e){
-        } catch (Exception e){
+        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
             fail("wrong exception");
         }
-		try{
-			pickup.setPickupRadius(-1);
+        try {
+            pickup.setPickupRadius(-1);
             fail("pickup range cannot be negative");
-        } catch (IllegalArgumentException e){
-        } catch (Exception e){
+        } catch (IllegalArgumentException e) {
+        } catch (Exception e) {
             fail("wrong exception");
         }
-	}
+    }
 }
