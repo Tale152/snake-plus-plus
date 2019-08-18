555ff29ae9f3942622432b9d7197d2e3fcb8fe99
Nixola <nicolaorlando24@gmail.com>
2019-08-18 18:15:41 +0200

    Javadoc and cleanup in ActionImpl

--- src/implementation/controller/game/ActionImpl.java
+++ src/implementation/controller/game/ActionImpl.java
@@ -4,7 +4,11 @@ import design.controller.game.Action;
 import design.model.game.Direction;
 import design.model.game.PlayerNumber;
 
-public class ActionImpl implements Action {
+/**
+ * @see Action
+ * @author Nicola Orlando
+ */
+public final class ActionImpl implements Action {
 
     private final PlayerNumber player;
     private final Direction direction;
@@ -19,6 +23,11 @@ public class ActionImpl implements Action {
         return this.direction;
     }
 
+    /**
+     * Creates an action representing a snake and the direction it should turn towards.
+     * @param n The player number.
+     * @param dir The direction their snake should turn towards.
+     */
     public ActionImpl(final PlayerNumber n, final Direction dir) {
         this.player = n;
         this.direction = dir;
