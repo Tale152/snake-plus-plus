5b353c46e23258bc5b635a97d1d5b0a8c8e199c7
Nixola <nicolaorlando24@gmail.com>
2019-08-18 17:55:21 +0200

    Added WorldDescriptor javadoc

--- src/design/controller/application/WorldDescriptor.java
+++ src/design/controller/application/WorldDescriptor.java
@@ -1,7 +1,26 @@
 package design.controller.application;
 
+/**
+ * A class to hold some informations about a world without loading it in its entirety.
+ * @author Alessandro Talmi
+ * @author Elisa Tronetti
+ * @author Nicola Orlando
+ */
 public interface WorldDescriptor {
+    /**
+     * Get the name of the world.
+     * @return The name of the world.
+     */
     String getName();
+    /**
+     * Get a small description of the world.
+     * @return The world description.
+     */
     String getDescription();
+    /**
+     * Get the name of the folder containing the world levels.
+     * Equal to the name of the world file, minus the extension.
+     * @return The name of the folder containing the world levels.
+     */
     String getFolderName();
 }
